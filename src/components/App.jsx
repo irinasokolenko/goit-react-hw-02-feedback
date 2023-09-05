eee70ee82322a085968233b09768f41921d1cb2e
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-09-05 13:30:17 +0300

    hw01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
+import React, { Component } from "react";
+
+import { Section } from `./Section`
+import { FeedbackOptions } from `./FeedbackOptions`
+import {Statistics} from `./Statistics`
+import { Notification } from `./Notification`
+
+export class App extends Component{
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+}
 export const App = () => {
   return (
     <div
